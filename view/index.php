2771077f3dd130d5506836950d07aff46534571d
Jose Pinto <jose123fpinto@gmail.com>
2022-10-01 17:36:39 -0600

    01/10/2022 subiendo a GitHub

--- view/index.php
+++ view/index.php
@@ -1,7 +1,10 @@
-echo "# PERSONAL_CRUD" >> README.md
+<h1>Login</h1>
+
+
+<!-- echo "# PERSONAL_CRUD" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/CardomDeveloper/PERSONAL_CRUD.git
-git push -u origin main
+git push -u origin main -->
